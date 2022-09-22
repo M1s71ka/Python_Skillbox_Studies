cc956163657834d49dc69e70c740b813a72c512c
Maxim Kudryakov <101654340+M1s71ka@users.noreply.github.com>
2022-09-22 13:58:50 +0500

    Add files via upload

---
+++ Python_Skillbox/3.6.py
@@ -0,0 +1,4 @@
+first_number = int(input('Введите первое число: '))
+second_number = int(input('Введите второе число: '))
+result = first_number % 100 + second_number % 100
+print('Сумма: ', result)

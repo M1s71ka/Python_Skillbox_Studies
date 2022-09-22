46f02c5cc18b7039b8ef12e031552570c9e881bb
Maxim Kudryakov <101654340+M1s71ka@users.noreply.github.com>
2022-09-22 13:49:04 +0500

    Add files via upload

---
+++ Python_Skillbox/3.4.py
@@ -0,0 +1,4 @@
+first_side = int(input('Введите значение первого катета: '))
+second_side = int(input('Введите значение второго катета: '))
+s = first_side * second_side / 2
+print('Площадь треугольника: ', s)

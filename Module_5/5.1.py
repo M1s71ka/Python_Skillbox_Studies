fb684c5df4dce269a770c18f01b4a9031726cc90
Maxim Kudryakov <101654340+M1s71ka@users.noreply.github.com>
2022-09-24 17:51:08 +0500

    Create 5.1.py

---
+++ Module_5/5.1.py
@@ -0,0 +1,9 @@
+experience = int(input('Введите кол-во опыта: '))
+if experience >= 5000:
+	print('Ваш уровень: 4')
+elif experience >= 2500:
+	print('Ваш уровень: 3')
+elif experience >= 1000:
+	print('Ваш уровень: 2')		
+else:
+	print('Ваш уровень: 1')
